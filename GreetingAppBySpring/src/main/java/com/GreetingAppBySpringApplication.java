b86e8dc9e48cfb1e563a19aae7986e915afa8472
Bhushan Patil <bhupatil001@gmail.com>
2021-04-21 10:57:23 +0530

    ADDED: greeting project created and adding dependencies

---
+++ GreetingAppBySpring/src/main/java/com/GreetingAppBySpringApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingAppBySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingAppBySpringApplication.class, args);
+	}
+
+}
